574e1126d3bbeed509e98f12a903b953e24ef926
David92p <davide.panetta@hotmail.com>
2021-02-20 19:36:48 +0100

    ADD exercise

---
+++ function/Exercise95.py
@@ -0,0 +1,31 @@
+
+
+#Capitalize It
+
+def capitalize(string):
+    string = string.lower()
+    my_list = list(string)
+    new_string = ""
+    if my_list[0] == my_list[0].lower():
+            my_list[0] = my_list[0].upper()
+    for i in range(len(my_list)):
+        if my_list[i] is not my_list[len(my_list)-1] and (my_list[i] == "." or my_list[i] == "?" or my_list[i] == "!"):
+            if  my_list[i+1] == " ":
+                my_list[i+2] = my_list[i+2].upper()
+            else:
+                my_list[i+1].upper()
+        if my_list[i] == "i":
+            if my_list[i-1] == " " and my_list[i+1] == " ":
+                my_list[i] = "I"
+            elif my_list[i-1] == " " and my_list[i+1] == "'" or my_list[i+1] == "?" or my_list[i+1] == "!" or my_list[i+1] == ".":
+                my_list[i] = "I"
+    new_string = new_string.join(my_list)
+    print(new_string)
+
+def main():
+    string = input("Enter your string ")
+    capitalize(string)
+    
+
+if __name__== "__main__":
+    main()
